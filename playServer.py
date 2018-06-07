0d4bfc4439b6e662b0600dcf2da44cb0ac7cd703
RikonYu <grtrbwsword@gmail.com>
2018-06-07 12:55:20 -0400

    wtf

--- playServer.py
+++ playServer.py
@@ -9,7 +9,7 @@ host='linux.cs.uwaterloo.ca'
 soc.bind((host,12346))
 soc.listen(5)
 disGame=None
-drones=DroneNet.DroneNet(Loading=False)
+drones=DroneNet.DroneNet()
 while(True):
     c,addr=soc.accept()
     while(True):
