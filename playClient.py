f9340bb29c6eb7cd3329ba354a7164f669cbf0b0
RikonYu <grtrbwsword@gmail.com>
2018-06-07 12:42:00 -0400

    server

--- playClient.py
+++ playClient.py
@@ -4,21 +4,24 @@ import util32
 import socket,os
 from pybrood import BaseAI, run, game, Color
 soc=None
-def send(u):
+def send(u,tp):
     msg=util32.game2msgDrone(u)
-    soc.send(pickle.dumps(msg))
+    soc.send([tp,pickle.dumps(msg)])
+def send_reg():
+    soc.send(['reg',util32.reg2msg()])
 def receive():
     k=soc.receive(10240)
     return pickle.loads(k)
 class PlayAI(BaseAI):
     def prepare(self):
         self.playerMe=game.self()
+        send_reg()
     def frame(self):
         if(game.getFrameCount()%10!=0):
             continue
         for i in game.getAllUnits():
             if(i.getType().getName()=='Zerg_Drone' and i.getPlayer()==self.playerMe):
-                send(i)
+                send(i,'drone')
                 k=receive()
                 util32.command(i,k)
     def finished(self):
--- playServer.py
+++ playServer.py
@@ -3,7 +3,7 @@ import DroneNet
 import util64
 import socket
 import pickle
-
+import random
 soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host='linux.cs.uwaterloo.ca'
 soc.bind((host,22))
@@ -20,7 +20,6 @@ data=c.recv(20480)
             if(k[0]=='reg'):
                 disGame=util64.gameInstance(k[0])
             else:
-                
                 ans=DroneNet.predict_ans(k)
-            
+                soc.send(pickle.dumps([random.randint()%360,random.randint()%360,random.randint()%6]))
             
