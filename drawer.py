6eff2af16ef1aa55c753f0dda714e1afdccc867c
RikonYu <grtrbwsword@gmail.com>
2018-06-12 20:05:50 -0400

    actual network

--- drawer.py
+++ drawer.py
@@ -2,11 +2,15 @@ import matplotlib.pyplot as plt
 import pickle
 import numpy
 
-fin=open('trainerr6.txt','rb')
-fin2=open('trainerr.txt','rb')
+fin=open('trainerr3.txt','rb')
+fin2=open('trainerr2.txt','rb')
+fin3=open('trainerr.txt','rb')
+
 k=pickle.load(fin)
 plt.plot(k,'r',label='deep')
 k=pickle.load(fin2)
 plt.plot(k,'b',label='shallow')
-
+k=pickle.load(fin3)
+plt.plot(k,'g',label='deeper')
+plt.legend()
 plt.show()
