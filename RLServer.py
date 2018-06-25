7145c050b8fa860d8324462f12fbced8606686a5
RikonYu <grtrbwsword@gmail.com>
2018-06-25 12:33:10 -0400

    debug

--- RLServer.py
+++ RLServer.py
@@ -9,69 +9,76 @@ from DragoonNet import DragoonNet
 from ClassConstr import getUnitClass
 import threading
 from consts import WINDOW_SIZE
-#Deep Q Learning
-batch_size=32
-disGame=None
-buf=ReplayBuffer.ReplayBuffer(20000)
-targetType=''
-training=threading.Semaphore(1)
-dragoons=None
-target=None
 
-epsilon=0.3
-discount=0.9
-learn_epoch=0
-lock=util64.RWLock()
+# Deep Q Learning
+batch_size = 32
+disGame = None
+buf = ReplayBuffer.ReplayBuffer(20000)
+targetType = ''
+dragoons = None
+target = None
+tempd=None
+
+epsilon = 0.3
+discount = 0.9
+learn_epoch = 0
+lock = util64.RWLock()
+
+
 def learner():
-    global dragoons,buf,disGame,target,discount,learn_epoch,targetType,lock
-    replace_every=500
-    while(True):
-        samples=buf.sample(batch_size)
-        if(len(samples)==0):
+    global dragoons, buf, disGame, target, discount, learn_epoch, targetType, lock
+    replace_every = 500
+    while (True):
+        samples = buf.sample(batch_size)
+        if (len(samples) == 0):
             time.sleep(2)
             continue
         print('training')
-        temp = getUnitClass(targetType)
-        temp.set_weights(dragoons.get_weights())
-        X=numpy.array([dragoons.msg2state(disGame,i) for i,_a,_sp,_r,_it in samples])
-        Y=dragoons.predict_all(X)
-        aprime=target.predict_max(numpy.array(dragoons.msg2state(disGame,i) for _s,_a,i,_r,_it in samples))
-        Y_=[(samples[i][3]+discount*aprime[i]*(1-samples[i][4])) for i in range(batch_size)]
-        diff=numpy.copy(Y)
+        lock.aquire_write()
+        tempd.set_weights(dragoons.get_weights())
+        lock.release_write()
+        X = numpy.array([dragoons.msg2state(disGame, i) for i, _a, _sp, _r, _it in samples])
+        Y = dragoons.predict_all(X)
+        aprime = target.predict_max(numpy.array(dragoons.msg2state(disGame, i) for _s, _a, i, _r, _it in samples))
+        Y_ = [(samples[i][3] + discount * aprime[i] * (1 - samples[i][4])) for i in range(batch_size)]
+        diff = numpy.copy(Y)
         for i in range(batch_size):
-            diff[i,samples[i][1][0],samples[i][1][1],samples[i][1][2]]+=Y_[i]
-        #new_agent.fit(X,diff)
+            diff[i, samples[i][1][0], samples[i][1][1], samples[i][1][2]] += Y_[i]
+        # new_agent.fit(X,diff)
 
-        temp.train(X,diff)
-        if(learn_epoch%replace_every==0):
+        temp.train(X, diff)
+        if (learn_epoch % replace_every == 0):
             temp.save()
             target.set_weights(temp.get_weights())
         lock.aquire_write()
-        #dragoons.set_weights(temp.get_weights())
+        dragoons.set_weights(temp.get_weights())
         lock.release_write()
-        learn_epoch+=1
+        learn_epoch += 1
+
+
 def unit_RL(con):
-    global disGame,buf,dragoons,epsilon,targetType,target
-    last_state=None
-    last_action=None
-    last_value=0
-    visited=numpy.zeros([1,1])
-    while(True):
+    global disGame, buf, dragoons, epsilon, targetType, target,tempd
+    last_state = None
+    last_action = None
+    last_value = 0
+    visited = numpy.zeros([1, 1])
+    while (True):
         try:
-            data=util64.recv_msg(con)
-            k=pickle.loads(data)
-            #print(k)
-            if(k[0]=='reg'):
-                disGame=util64.gameInstance(k[1])
-                targetType=k[2]
+            data = util64.recv_msg(con)
+            k = pickle.loads(data)
+            # print(k)
+            if (k[0] == 'reg'):
+                disGame = util64.gameInstance(k[1])
+                targetType = k[2]
                 dragoons = getUnitClass(targetType, True)
                 target = getUnitClass(targetType, True)
+                tempd = getUnitClass(targetType)
                 con.send(b'ok')
                 break
             else:
-                ans=0
+                ans = 0
                 X = dragoons.msg2state(disGame, k[1])
-                if (k[0]=='terminal' and last_action != None):
+                if (k[0] == 'terminal' and last_action != None):
                     if (k[1][1][1] != last_value):
                         print('reward', last_value, k[1][1][1])
                     buf.add(last_state, last_action, X, (k[1][1][1] - last_value), 1)
@@ -79,33 +86,39 @@ def unit_RL(con):
                     last_action = ans
                     last_value = k[1][1][1]
                     break
-                if(visited.shape[0]==1):
-                    visited=numpy.zeros(disGame.regions.shape)
-                #print(k)
+                if (visited.shape[0] == 1):
+                    visited = numpy.zeros(disGame.regions.shape)
+                # print(k)
                 visited[k[1][0][0], k[1][0][1]] += 1
-                if(numpy.random.random()<epsilon):
-                    places=dragoons.msg2mask(disGame,k[1])
-                    #probs=numpy.exp(-visited[])
-                    probs=numpy.zeros([WINDOW_SIZE,WINDOW_SIZE])
-                    x=k[1][0][0]
-                    y=k[1][0][1]
+                if (numpy.random.random() < epsilon):
+                    places = dragoons.msg2mask(disGame, k[1])
+                    # probs=numpy.exp(-visited[])
+                    probs = numpy.zeros([WINDOW_SIZE, WINDOW_SIZE])
+                    x = k[1][0][0]
+                    y = k[1][0][1]
                     ax = max(0, WINDOW_SIZE // 2 - x)
                     ay = max(0, WINDOW_SIZE // 2 - y)
                     probs[ax:min(WINDOW_SIZE, visited.shape[0] - x + WINDOW_SIZE // 2),
-                          ay:min(WINDOW_SIZE, visited.shape[1] - y + WINDOW_SIZE // 2)] = numpy.exp(-visited[
-                                                                        max(0, x - WINDOW_SIZE // 2):min(x + WINDOW_SIZE // 2, visited.shape[0]),
-                                                                        max(0, y - WINDOW_SIZE // 2):min(y + WINDOW_SIZE // 2, visited.shape[1])])
-                    ini,inj,ink=numpy.nonzero(places)
-                    #top5=numpy.argpartition(probs[ini,inj],-5)[-5:]
-                    #print('options',numpy.unravel_index(top5,probs.shape),probs[ini,inj][top5],len(ini))
-                    #ind=numpy.random.choice(5,p=probs[ini,inj][top5]/sum(probs[ini,inj][top5]))
-                    ind=numpy.argmax(probs[ini,inj])
-                    #ans=[ini[ind]-WINDOW_SIZE//2,inj[ind]-WINDOW_SIZE//2,ink[ind]]
+                    ay:min(WINDOW_SIZE, visited.shape[1] - y + WINDOW_SIZE // 2)] = numpy.exp(-visited[
+                                                                                               max(0,
+                                                                                                   x - WINDOW_SIZE // 2):min(
+                                                                                                   x + WINDOW_SIZE // 2,
+                                                                                                   visited.shape[0]),
+                                                                                               max(0,
+                                                                                                   y - WINDOW_SIZE // 2):min(
+                                                                                                   y + WINDOW_SIZE // 2,
+                                                                                                   visited.shape[1])])
+                    ini, inj, ink = numpy.nonzero(places)
+                    # top5=numpy.argpartition(probs[ini,inj],-5)[-5:]
+                    # print('options',numpy.unravel_index(top5,probs.shape),probs[ini,inj][top5],len(ini))
+                    # ind=numpy.random.choice(5,p=probs[ini,inj][top5]/sum(probs[ini,inj][top5]))
+                    ind = numpy.argmax(probs[ini, inj])
+                    # ans=[ini[ind]-WINDOW_SIZE//2,inj[ind]-WINDOW_SIZE//2,ink[ind]]
                     ans = [ini[ind] - WINDOW_SIZE // 2, inj[ind] - WINDOW_SIZE // 2, ink[ind]]
-                    #print(ans)
+                    # print(ans)
                 else:
-                    #temps = getUnitClass()
-                    #temps.set_weights(dragoons.get_weights())
+                    # temps = getUnitClass()
+                    # temps.set_weights(dragoons.get_weights())
                     mask = dragoons.msg2mask(disGame, k[1])
                     '''
                     ftest=open('masks.txt','wb')
@@ -113,22 +126,24 @@ def unit_RL(con):
                     ftest.close()
                     '''
                     lock.acquire_read()
-                    ans=dragoons.predict_ans_masked(X,mask)
+                    ans = dragoons.predict_ans_masked(X, mask)
                     lock.release_read()
                 con.sendall(pickle.dumps(ans))
-                if(last_action!=None):
-                    if(k[1][1][1]!=last_value):
-                        print('reward',last_value,k[1][1][1])
-                    buf.add(last_state,last_action,X,(k[1][1][1]-last_value),0)
-                last_state=X
-                last_action=ans
-                last_value=k[1][1][1]
+                if (last_action != None):
+                    if (k[1][1][1] != last_value):
+                        print('reward', last_value, k[1][1][1])
+                    buf.add(last_state, last_action, X, (k[1][1][1] - last_value), 0)
+                last_state = X
+                last_action = ans
+                last_value = k[1][1][1]
         except EOFError:
             break
-if(__name__=='__main__'):
-    soc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    host='linux.cs.uwaterloo.ca'
-    soc.bind((host,12346))
+
+
+if (__name__ == '__main__'):
+    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'linux.cs.uwaterloo.ca'
+    soc.bind((host, 12346))
     soc.listen(5)
     lx = threading.Thread(target=learner, args=[])
     time.sleep(1)
@@ -136,8 +151,8 @@ if(__name__=='__main__'):
 
     print('listening')
 
-    while(True):
-        con,addr=soc.accept()
-        k=threading.Thread(target=unit_RL,args=[con])
+    while (True):
+        con, addr = soc.accept()
+        k = threading.Thread(target=unit_RL, args=[con])
         time.sleep(1)
         k.start()
