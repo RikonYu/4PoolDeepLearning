4fb6f7935d1a9ab29ee390af7600348b38b723cc
RikonYu <grtrbwsword@gmail.com>
2018-06-24 23:38:40 -0400

    debug

--- RLServer.py
+++ RLServer.py
@@ -78,10 +78,9 @@ def unit_RL(con):
                           ay:min(WINDOW_SIZE, visited.shape[1] - y + WINDOW_SIZE // 2)] = numpy.exp(-visited[
                                                                         max(0, x - WINDOW_SIZE // 2):min(x + WINDOW_SIZE // 2, visited.shape[0]),
                                                                         max(0, y - WINDOW_SIZE // 2):min(y + WINDOW_SIZE // 2, visited.shape[1])])
-                    print('total',numpy.sum(probs),x,y)
                     ini,inj,ink=numpy.nonzero(places)
                     top5=numpy.argpartition(probs[ini,inj],-5)[-5:]
-                    print('options',top5,probs[ini,inj][top5],len(ini))
+                    print('options',numpy.unravel_index(top5,probs.shape),probs[ini,inj][top5],len(ini))
                     ind=numpy.random.choice(5,p=probs[ini,inj][top5]/sum(probs[ini,inj][top5]))
                     #ans=[ini[ind]-WINDOW_SIZE//2,inj[ind]-WINDOW_SIZE//2,ink[ind]]
                     ans = [ini[top5[ind]] - WINDOW_SIZE // 2, inj[top5[ind]] - WINDOW_SIZE // 2, ink[ind]]
