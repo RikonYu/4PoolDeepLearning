67587a31b4466c0288692c2318b00704d9e536a0
RikonYu <grtrbwsword@gmail.com>
2018-07-08 23:23:50 -0400

    fml

--- ClassConstr.py
+++ ClassConstr.py
@@ -1,6 +1,7 @@
 from DragoonNet import DragoonNet
 from DroneNet import DroneNet
 def getUnitClass(name,loading=False):
+    print('name')
     if(name=='Protoss_Dragoon'):
         return DragoonNet(loading)
     if(name=='Zerg_Drone'):
