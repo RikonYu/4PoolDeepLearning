0f4e7c93bcbfdfd933b6fd8ea26fa5148f44fb58
RikonYu <grtrbwsword@gmail.com>
2018-07-12 19:05:04 -0400

    debug

--- QLearning.py
+++ QLearning.py
@@ -108,6 +108,7 @@ class QLearning:
                     con.send(b'ok')
                     break
                 else:
+                    print(self.disGame.regions.shape)
                     X = self.units.msg2state(self.disGame, k[1])
                     if (k[0] == 'terminal' and last_action is not None):
                         self.buflock.acquire()
