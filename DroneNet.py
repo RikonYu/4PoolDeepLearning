9677da2372d6f31bcde81fbde9da3fa4a5dd2e63
RikonYu <grtrbwsword@gmail.com>
2018-07-19 01:18:38 -0400

    new idea

--- DroneNet.py
+++ DroneNet.py
@@ -34,7 +34,7 @@ class DroneNet(UnitNet):
                 self.deconv2 = deconv_block(Concatenate(axis=3)([self.up1, self.conv2]), 1)
                 self.up3 = UpSampling2D((2, 2))(self.deconv2)
                 self.deconv4 = Conv2DTranspose(64, (3, 3), activation='relu', padding='same')(self.up3)
-                self.out = Conv2DTranspose(DroneNet._out_channel, (3, 3), activation=output_type, padding='same')(
+                self.out = Conv2DTranspose(DroneNet._out_channel, (1,1), activation=output_type, padding='same')(
                     self.deconv4)
                 self.model = Model(inputs=self.inp, outputs=self.out)
                 #optz=Adam(0.001)
