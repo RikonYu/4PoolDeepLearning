30a452d9257613641d9c3ef268dfe3855044da2b
RikonYu <grtrbwsword@gmail.com>
2018-07-22 00:43:39 -0400

    asd

--- DebugLearner.py
+++ DebugLearner.py
@@ -51,6 +51,9 @@ class DebugLearner(Learner):
                         Y=numpy.zeros([WINDOW_SIZE,WINDOW_SIZE,self.units._out_channel])
                         for ind,_ in numpy.ndenumerate(Y[:,:,1]):
                             Y[ind[0],ind[0],1]=-numpy.linalg.norm([ind[0]-pos[0], ind[1]-pos[1]])/32
+                        ftarget=open('target.txt','w')
+                        pickle.dump(Y, ftarget)
+                        ftarget.close()
                         history=self.units.train(X.reshape([-1, WINDOW_SIZE, WINDOW_SIZE,self.units._in_channel]), Y.reshape([-1,WINDOW_SIZE,WINDOW_SIZE,self.units._out_channel]))
                         self.ferr.write(str(history.history['loss'][0])+'\n')
                         self.ferr.flush()
