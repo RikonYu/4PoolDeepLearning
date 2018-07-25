c16bc000f53f192316b614216dbbadaf14fcbd85
RikonYu <grtrbwsword@gmail.com>
2018-07-24 23:45:16 -0400

    asd

--- RLServer.py
+++ RLServer.py
@@ -10,13 +10,11 @@ from QLearning import QLearning
 from consts import WINDOW_SIZE
 from DebugLearner import DebugLearner
 
-
-#agent=QLearning(0.3,0.9,0,64)
-#agent=A2C(0.3,0.95,0,0)
 if (__name__ == '__main__'):
     soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     #agent = DebugLearner(0.3, 0.9, 0, 64)
-    agent=QLearning(0.3,0.9,0,64)
+    #agent=QLearning(0.3,0.9,0,64)
+    agent = A2C(0.3, 0.95, 0, 0)
     host = 'linux.cs.uwaterloo.ca'
     soc.bind((host, 12346))
     soc.listen(5)
