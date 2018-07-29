d55ddae2dfe96ff40592e1bd4bb502db012363cd
RikonYu <grtrbwsword@gmail.com>
2018-07-29 00:17:42 -0400

    asd

--- QLearning.py
+++ QLearning.py
@@ -130,6 +130,7 @@ class QLearning(Learner):
                     visited[msg.myInfo.coord[0], msg.myInfo.coord[1]] += 1
                     if (visited[msg.myInfo.coord[0], msg.myInfo.coord[1]] == 1):
                         unvisited -= 1
+                    print('epsilon%d'%self.epsilon)
                     if (numpy.random.random() < self.epsilon):
                         if(numpy.random.random()<pSticky):
                             print('sticky')
